98db279a73245f42a1f6c39b186dfb79adec64e6
Saba Jmukhadze <sabajmukhadze23@gmail.com>
2024-11-12 09:16:36 +0400

    DB file logic seperated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./db");
 const authRoutes = require("./routes/authRoutes");
 const blogRoutes = require("./routes/blogRoutes");
 const commentRoutes = require("./routes/commentRoutes");
@@ -12,16 +13,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = "mongodb://localhost:27017/blog-db";
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
